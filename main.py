621c798c3508a74433607b7e90f8de9d99000b6b
nguyenquangdien-gmo <diennq.runsystem@gmail.com>
2026-06-19 16:31:39 +0700

    change port

--- main.py
+++ main.py
@@ -109,4 +109,4 @@ def serve_dashboard():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=24572, reload=True)
